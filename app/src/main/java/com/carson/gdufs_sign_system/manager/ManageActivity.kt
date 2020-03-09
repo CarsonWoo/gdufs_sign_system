1b33e61c639a33503bb05e48b0d585c5cb0d7c0b
CarsonWoo <845947690@qq.com>
2020-03-09 22:09:59 +0800

    commit manage activity files

--- app/src/main/java/com/carson/gdufs_sign_system/manager/ManageActivity.kt
+++ app/src/main/java/com/carson/gdufs_sign_system/manager/ManageActivity.kt
@@ -2,8 +2,11 @@ package com.carson.gdufs_sign_system.manager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import com.carson.gdufs_sign_system.R
 import com.carson.gdufs_sign_system.base.BaseActivity
 import com.carson.gdufs_sign_system.utils.StatusBarUtil
@@ -11,6 +14,8 @@ import com.carson.gdufs_sign_system.utils.StatusBarUtil
 class ManageActivity : BaseActivity(), IViewCallback {
 
     private lateinit var mController: ManageController
+
+    private lateinit var mLogoutButton: ImageView
 
     override fun getContentViewResId(): Int {
         return R.layout.activity_manage
@@ -26,10 +31,25 @@ class ManageActivity : BaseActivity(), IViewCallback {
     }
 
     private fun initView() {
+        mLogoutButton = findViewById(R.id.btn_logout)
         mController = ManageController(this, this)
     }
 
     private fun initEvent() {
+        mLogoutButton.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setMessage("确定退出登录么")
+                .setPositiveButton("确定") {
+                    dialog, _ ->
+                    Log.i(TAG, "logout")
+                    dialog.dismiss()
+                }
+                .setNegativeButton("取消") {
+                    dialog, _ -> dialog.dismiss()
+                }
+                .setCancelable(true)
+                .show()
+        }
     }
 
     override fun onShowText(str: String, type: Int) {
